a868971fe25a7e8d5e33069911a36653302f8741
Ramires <moreira.ramires@gmail.com>
2022-06-15 11:49:58 -0300

    alterando elementos

--- js/main.js
+++ js/main.js
@@ -2,37 +2,21 @@
 const robotron = document.getElementById('robotron');
 const controlesAjuste = document.querySelectorAll('.controle-ajuste');
 const pecaTitulo= document.querySelectorAll('.peca-titulo')
-let braco = document.querySelector('.controle input');
 
 
 controlesAjuste.forEach(
   (element)=>{
-    element.addEventListener("click",()=>{
-      manipulaDados(element.textContent)
+    
+    element.addEventListener("click",(event)=>{
+
+      let controle = event.target
+      
+      manipulaDados(controle.textContent,controle.parentNode)
+
+
     })
   }
 )
-
-
-pecaTitulo.forEach(
-
-  (element)=>{
-    let nomePeca = element.innerHTML
-    nomePeca= nomePeca.toLowerCase()
-    nomePeca= nomePeca.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-
-    element.setAttribute('id',`controle-contador-${nomePeca}`) //label
-
-
-    
-  
- 
-  }
-)
-
-
-
-
 
 // input.createAttribute("id")
 // input.setAttribute('MeuID')
@@ -42,11 +26,14 @@ pecaTitulo.forEach(
 
 
 
-function manipulaDados(operacao){
+function manipulaDados(operacao,controle){
+  let peca = controle.querySelector('.controle-contador');
+  console.log(controle)
+
 
   if(operacao ==="-"){
-    braco.value = parseInt(braco.value) -1 
+    peca.value = parseInt(peca.value) -1 
   } else if (operacao ==="+"){
-    braco.value = parseInt(braco.value) +1 
+    peca.value = parseInt(peca.value) +1 
   } 
 }
